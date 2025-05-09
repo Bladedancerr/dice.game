fc5cbad3d0f574a9c93c49c10b12535ecf541955
Bladedancerr <lashaghotvadze@gmail.com>
2025-05-09 20:16:48 +0400

    generic exception handling
    added generic exception handling

--- src/main/java/com/lasha/dice/game/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/lasha/dice/game/exceptionHandler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.lasha.dice.game.exception.InvalidUserValuesException;
 import com.lasha.dice.game.exception.UserAlreadyExistsException;
 import com.lasha.dice.game.exception.UserNotFoundException;
 import com.lasha.dice.game.exception.UsersNotAvailableException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,6 +41,13 @@ public class GlobalExceptionHandler
     {
         ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), request.getRequestURI());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, HttpServletRequest request)
+    {
+        ErrorResponse error = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), request.getRequestURI());
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
